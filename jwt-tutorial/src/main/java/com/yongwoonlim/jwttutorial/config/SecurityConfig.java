1f82babbf48b5412644064aa4f331664bff163e2
Yongwoon Lim <eng.yowoo@gmail.com>
2021-08-30 19:41:28 +0900

    JWT, Security 설정 추가

--- jwt-tutorial/src/main/java/com/yongwoonlim/jwttutorial/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/com/yongwoonlim/jwttutorial/config/SecurityConfig.java
@@ -1,12 +1,36 @@
 package com.yongwoonlim.jwttutorial.config;
 
+import com.yongwoonlim.jwttutorial.jwt.JwtAccessDeniedHandler;
+import com.yongwoonlim.jwttutorial.jwt.JwtAuthenticationEntryPoint;
+import com.yongwoonlim.jwttutorial.jwt.JwtSecurityConfig;
+import com.yongwoonlim.jwttutorial.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -15,8 +39,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
-                .antMatchers("/api/hello").permitAll() // /api/hello에 대한 요청은 인증없이 접근 허용
-                .anyRequest().authenticated(); // 나머지는 인증을 통해 접근
+        http.csrf().disable()
+            .exceptionHandling()
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+            .and()
+            .headers()
+            .frameOptions()
+            .sameOrigin()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한 설정
+            .antMatchers("/api/hello").permitAll() // /api/hello에 대한 요청은 인증없이 접근 허용
+            .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/signup").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
---
+++ jwt-tutorial/src/main/java/com/yongwoonlim/jwttutorial/jwt/TokenProvider.java
@@ -0,0 +1,86 @@
+package com.yongwoonlim.jwttutorial.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class TokenProvider {
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+
+    private final long tokenValidityInMillis;
+    private final Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secret,
+                         @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSec) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.tokenValidityInMillis = tokenValidityInSec * 1000;
+    }
+
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        Date validity = new Date(System.currentTimeMillis() + this.tokenValidityInMillis);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            logger.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (MalformedJwtException | IllegalArgumentException e) {
+            logger.info("잘못된 JWT 토큰입니다.");
+        } catch (SignatureException e) {
+            logger.info("잘못된 JWT 서명입니다.");
+        }
+        return false;
+    }
+}
