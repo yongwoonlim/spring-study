c448835cf5a05395bfbaa2f701ff59411c236e36
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-09 01:39:42 +0900

    HelloWorldController 생성 및 구현

---
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.yongwoonlim.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
