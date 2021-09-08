79a0c58fa271b5df7d6b51883bd8f6e3b347e1db
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-09 01:39:30 +0900

    HelloWorldBean 클래스 생성 및 구현

---
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.yongwoonlim.restfulwebservice;
+
+// lombok
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
