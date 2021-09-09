94901a853700ecf5ba4646252094b2ee4c95b596
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-09 09:51:17 +0900

    사용자를 표현한 User 클래스 추가

---
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/user/User.java
@@ -0,0 +1,16 @@
+package com.yongwoonlim.restfulwebservice.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private Date joinDate;
+}
