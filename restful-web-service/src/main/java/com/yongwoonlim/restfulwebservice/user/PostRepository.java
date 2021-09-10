4a58a07ace9f79ca8917f33e6c75a09753921e89
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 04:43:10 +0900

    post repository 추가

--- restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/user/PostRepository.java
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/user/PostRepository.java
@@ -1,2 +1,8 @@
-package com.yongwoonlim.restfulwebservice.user;public interface PostRepository {
+package com.yongwoonlim.restfulwebservice.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
