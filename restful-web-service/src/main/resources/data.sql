9479139d52714e4e4ed5ec0dff3aa360e11e96de
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 03:21:27 +0900

    테스트용 데이터 아이디 변경

--- restful-web-service/src/main/resources/data.sql
+++ restful-web-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user values(1, sysdate(), 'User1', 'test1111', '901010-11111111');
-INSERT INTO user values(2, sysdate(), 'User2', 'test2222', '801010-11111111');
-INSERT INTO user values(3, sysdate(), 'User3', 'test3333', '701010-11111111');
+INSERT INTO user values(90001, sysdate(), 'User1', 'test1111', '901010-11111111');
+INSERT INTO user values(90002, sysdate(), 'User2', 'test2222', '801010-11111111');
+INSERT INTO user values(90003, sysdate(), 'User3', 'test3333', '701010-11111111');
