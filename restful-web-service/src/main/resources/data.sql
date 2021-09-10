b52a6ae3da5b9ddafd6ee40bbd24d4451ea47fa1
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 04:23:26 +0900

    테스트용 posts 데이터 삽입 쿼리 추가

--- restful-web-service/src/main/resources/data.sql
+++ restful-web-service/src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO user values(90001, sysdate(), 'User1', 'test1111', '901010-11111111');
-INSERT INTO user values(90002, sysdate(), 'User2', 'test2222', '801010-11111111');
-INSERT INTO user values(90003, sysdate(), 'User3', 'test3333', '701010-11111111');
+INSERT INTO user VALUES(90001, sysdate(), 'User1', 'test1111', '901010-11111111');
+INSERT INTO user VALUES(90002, sysdate(), 'User2', 'test2222', '801010-11111111');
+INSERT INTO user VALUES(90003, sysdate(), 'User3', 'test3333', '701010-11111111');
+
+INSERT INTO post VALUES(10001, 'My first post', 90001);
+INSERT INTO post VALUES(10002, 'My second post', 90001);
