55d48c67eca8e72e613cbc54ac1e0331fbecb9bd
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 02:11:18 +0900

    h2 console에 누구나 접근 가능하도록 security config 수정

--- restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/config/SecurityConfig.java
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/config/SecurityConfig.java
@@ -3,12 +3,21 @@ package com.yongwoonlim.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .and()
+                .headers().frameOptions().disable();
+    }
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception{
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
         builder.inMemoryAuthentication()
                 .withUser("kenneth")
                 .password("{noop}1234")
