db3a570473d7604954e042621628d38293300629
Yongwoon Lim <eng.yowoo@gmail.com>
2021-09-11 03:21:54 +0900

    user jpa controller 추가

---
+++ restful-web-service/src/main/java/com/yongwoonlim/restfulwebservice/user/UserJpaController.java
@@ -0,0 +1,42 @@
+package com.yongwoonlim.restfulwebservice.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/jpa/users")
+public class UserJpaController {
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User retrieveUserById(@PathVariable int id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(String.format("ID[%d] not found", id)));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(savedUser.getId())
+                        .toUri()).build();
+    }
+}
